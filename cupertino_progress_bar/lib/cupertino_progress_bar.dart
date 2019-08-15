10297ca1c67f5f3b31c36b5443d6e26345bc4402
Tobe O <thosakwe@gmail.com>
2019-08-15 04:18:12 -0400

    Init cupertino progress bar

---
+++ cupertino_progress_bar/lib/cupertino_progress_bar.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CupertinoProgressBar extends StatefulWidget {
+  final double value;
+
+  const CupertinoProgressBar({Key key, this.value}) : super(key: key);
+
+  @override
+  CupertinoProgressBarState createState() => CupertinoProgressBarState();
+}
+
+class CupertinoProgressBarState extends State<CupertinoProgressBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
